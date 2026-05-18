1fa2cc595bc4052e811d5f50767d5d5ee05b9cae
Gregorino <gregapo@gmail.com>
2026-05-18 23:54:57 +0200

    Simplify setup.py: use pyproject.toml as single source of truth"

--- setup.py
+++ setup.py
@@ -1,24 +1,8 @@
-import setuptools
+"""
+Compatibility shim for setuptools.
+For build configuration, see pyproject.toml
+"""
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="TrainBooster",
-    version="1.0.0",
-    author="Gregor Podrekar",
-    author_email="gregapo@gmail.com",
-    description="Something to boost your DL training:)",
-    long_description="",
-    long_description_content_type="",
-    url="https://github.com/Gregorino/TrainBooster/",
-    packages=setuptools.find_packages('.'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    include_package_data=True,
-    package_data={'': ['audio.mp3']},
-    install_requires=['playsound']
-)
+if __name__ == "__main__":
+    setup()
